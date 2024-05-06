12b410be1795ffe84428fd1aa5265d6fedd28737
Milagros Soberon <msoberon@alu.frlp.utn.edu.ar>
2024-05-06 11:00:30 -0300

    Actualice el bloque de manejo de errores

--- Modulo1/04Clase/ejercicio-Soberon/server.js
+++ Modulo1/04Clase/ejercicio-Soberon/server.js
@@ -1,26 +1,25 @@
-const http = require('http');
+const http = require("http");
 const PORT = 3008;
 
 const server = http.createServer((request, response) => {
-    let respuesta = ""
-    let statusCode = 200;
-    if (request.url === '/') {
-        respuesta = "<h1>Bienvenid@s a nuestra web!</h1>";
-    } else if(request.url === '/cursos'){
-        respuesta = "<h1>Bienvenid@s a nuestra sección de cursos</h1>"
-    }else if(request.url === '/contacto'){
-        respuesta = "<h1>Bienvenid@s a nuestra sección de contacto!</h1>";
+  let respuesta = "";
+  let statusCode = 200;
+  if (request.url === "/") {
+    respuesta = "<h1>Bienvenid@s a nuestra web!</h1>";
+  } else if (request.url === "/cursos") {
+    respuesta = "<h1>Bienvenid@s a nuestra sección de cursos</h1>";
+  } else if (request.url === "/contacto") {
+    respuesta = "<h1>Bienvenid@s a nuestra sección de contacto!</h1>";
+  } else {
+    statusCode = 404;
+    response.setHeader("Content-Type", "text/plain; charset=utf-8");
+    respuesta = "Error 404: No se encuentra la ruta o recurso solicitado.";
+  }
+  response.statusCode = statusCode;
+  response.setHeader("Content-Type", "text/html; charset=utf-8");
+  response.end(respuesta);
+});
 
-    } else {
-        statusCode= 404;
-        respuesta = "<h1>No se encontro la ruta :( </h1>"
-    }
-    response.statusCode = statusCode;
-    response.setHeader('Content-Type', 'text/html; charset=utf-8');
-    response.end(respuesta);
-
-})
-
-server.listen(PORT, () => { 
-    console.log(`Servidor corriendo en el puerto ${PORT}`)
-})
+server.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
