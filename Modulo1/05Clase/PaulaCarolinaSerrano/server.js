05a54735a7df04dd2a937cf3f9ca1050bc5ef20b
Maria Elisa Araya <42949801+mariaelisaaraya@users.noreply.github.com>
2024-05-07 17:38:08 -0300

    Update server.js

--- Modulo1/05Clase/PaulaCarolinaSerrano/server.js
+++ Modulo1/05Clase/PaulaCarolinaSerrano/server.js
@@ -7,20 +7,16 @@ const PORT = 3050;
 app.get('/', (req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.send('"<h1>Bienvenidas a nuestra web</h1>"');
-    
-    
 });
 
 app.get('/cursos', (req, res) =>{
     res.setHeader('Content-Type', 'text/html');
     res.send("<h1>Bienvenidas a nuestra sección cursos</h1>");
-    
 });
 
 app.get('/contacto', (req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.send("<h1>Contactate con nosotras</h1>");
-    
 });
 
 //Ruta predeterminada para manejar rutas inexistentes (es un middleware)
@@ -28,7 +24,6 @@ app.use((req, res) => {
     let json_error = {"error": "404", "description":"No se encuentra la ruta o recurso solicitado"}
     res.setHeader('Content-Type', 'application/json');
     res.status(404).send(json_error);
-    
 });
 
 
