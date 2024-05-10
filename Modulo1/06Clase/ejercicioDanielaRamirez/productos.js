3299c385c8ed95f71c1957869359aea7a0618899
Daniela <randrade.daniela@gmail.com>
2024-05-09 22:36:15 -0400

    feat: adding homework of class 6

---
+++ Modulo1/06Clase/ejercicioDanielaRamirez/productos.js
@@ -0,0 +1,42 @@
+module.exports = [
+    {
+        imagen: "🍕",
+        nombre: "Pizza",
+        precio: 20
+    },
+    {
+        imagen: "🍔",
+        nombre: "Hamburguesa",
+        precio: 50
+    },
+    {
+        imagen: "🍟",
+        nombre: "Papas Fritas",
+        precio: 10
+    },
+    {
+        imagen: "🍿",
+        nombre: "Popcorn",
+        precio: 30
+    },
+    {
+        imagen: "🌭",
+        nombre: "Hot Dog",
+        precio: 40
+    },
+    {
+        imagen: "🍦",
+        nombre: "Helado",
+        precio: 60
+    },
+    {
+        imagen: "🍩",
+        nombre: "Dona",
+        precio: 40
+    },
+    {
+        imagen: "🍰",
+        nombre: "Pastel",
+        precio: 55
+    },
+];
