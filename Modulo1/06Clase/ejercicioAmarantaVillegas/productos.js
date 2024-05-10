b8e1b835b284697bafcd5ff92ea13519e4c186bb
amarantaVC <50127309+amarantaVC@users.noreply.github.com>
2024-05-09 22:36:51 -0400

    Entrega tarea 6

---
+++ Modulo1/06Clase/ejercicioAmarantaVillegas/productos.js
@@ -0,0 +1,27 @@
+module.exports = [
+    {
+        imagen: "🕶️",
+        nombre: "lentes de sol",
+        precio: 220
+    },
+    {
+        imagen: "🩳",
+        nombre: "shorts",
+        precio: 200
+    },
+    {
+        imagen: "👖",
+        nombre: "pantalon",
+        precio: 280
+    },
+    {
+        imagen: "👕",
+        nombre: "remera",
+        precio: 350
+    },
+    {
+        imagen: "🎩",
+        nombre: "sombrero",
+        precio: 320
+    }
+]
