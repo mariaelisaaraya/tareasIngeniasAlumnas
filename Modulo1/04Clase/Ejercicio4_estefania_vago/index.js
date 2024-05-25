5b3910f26732e345f346c0ae7e81676b93276fd2
Estefania Vago <ecvago@hotmail.com>
2024-05-24 21:15:14 -0300

    tarea 4

--- Modulo1/04Clase/Ejercicio4_estefania_vago/index.js
+++ Modulo1/04Clase/Ejercicio4_estefania_vago/index.js
@@ -4,33 +4,26 @@ const http = require ('http');
 const PORT = 3008;
 //Crear servidor
 const server = http.createServer((request, response)=>{
-    let statusCode=200;
-    let respuesta ="";
+    response.statusCode=200;
+    l
     
     if (request.url === '/'){
-        response.setHeader('charset','utf-8');
-        response.setHeader('Content-type','text/html');
-        respuesta = "<h1>Bienvenidas a la web</h1>";
+        response.setHeader("Content-Type", "text/html; charset=utf-8");
+        response.end("<h1>Bienvenidas a la web</h1>");
         
     }else if (request.url === '/cursos'){
-        response.setHeader('charset','utf-8');
-        response.setHeader('Content-type','text/html');
-        respuesta = "<h1>Bienvenidas! Acá verás nuestros cursos</h1>";
+        response.setHeader("Content-Type", "text/html; charset=utf-8");
+        response.end("<h1>Bienvenidas! Acá verás nuestros cursos</h1>");
 
-    }else if (request.url === '/contacto'){
-        response.setHeader('charset','utf-8');
-        response.setHeader('Content-type','text/html');
-        respuesta = "<h1>Contactanos por estos medios</h1>";
+    }else if (request.url === 'contacto'){
+        response.setHeader("Content-Type", "text/html; charset=utf-8");
+        response.end("<h1>Contactanos por estos medios</h1>");
     }else{
         statusCode = 404;
-        response.setHeader('charset','utf-8');
-        response.setHeader('Content-type','text/plain');
-        respuesta = "Error 404";
+        response.setHeader("Content-Type", "text/plain; charset=utf-8");
+        response.end("Error 404");
     }
-    response.statusCode=statusCode;
-    response.end(respuesta);
-
-})
-server.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`)
+    server.listen(PORT, () => {
+        console.log(´Servidor corriendo en el puerto ${PORT}´);
+      });
 })
