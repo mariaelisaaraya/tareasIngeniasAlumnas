80f864263d24c3ec259fc961e2156f33a7c1266a
Estefania Vago <ecvago@hotmail.com>
2024-05-27 13:03:44 -0300

    cuartaclase

---
+++ Modulo1/04Clase/Ejercicio4_EstefiVago/index.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const PORT=3001;
+
+const server = http.createServer((request, response) =>{
+    let respuesta = ""
+    let statusCode = 200
+    if (request.url === "/") {
+        respuesta = "<h1>Bienvenid@s!</h1>"
+    } else if (request.url === "/cursos") {
+        respuesta = "<h1>Bienvenid@s a la seccion de cursos.</h1>"
+    } else if (request.url === "/contacto") {
+        respuesta = "<h1>Contactanos por estos medios</h1>"
+    } else {
+        statusCode = 404
+        respuesta = "<h1>Pagina no encontrada</h1>"
+    }
+    
+    response.statusCode = statusCode
+    response.setHeader('charset', 'UTF-8')
+    response.setHeader('Content-Type', 'text/html')
+    response.end(respuesta)
+    server.listen(PORT, () => {
+        console.log(`Servidor escuchando en http://localhost:${PORT}`)
+    })
+
+
+});
