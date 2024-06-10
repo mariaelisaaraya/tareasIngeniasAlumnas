a1c04be2bc155fc6dd5679dc9ce8194ad8fec52e
macaferro <167114274+macaferro@users.noreply.github.com>
2024-06-10 08:34:42 -0300

    Delete Modulo1/07Clase/ejercicioMacarenaFerro/cursos.js
    Se elimino cursos.js

--- Modulo1/07Clase/ejercicioMacarenaFerro/cursos.js
+++
@@ -1,9 +0,0 @@
-const cursos = [{id: 1, nombre: 'javascript', categoria: 'programación'},
-                {id: 1, nombre: 'react js', categoria: 'programación'},
-                {id: 3, nombre: 'vue js', categoria: 'programación'},
-                {id: 4, nombre: 'sql', categoria: 'datos'},
-                {id: 5, nombre: 'mongodb', categoria: 'datos'},
-                {id: 6, nombre: 'ecommerce', categoria: 'producto'},
-                {id: 7, nombre: 'customer experience', categoria: 'producto'}];
-
-module.exports = cursos;
